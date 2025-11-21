4cb5a38fb16f13d846a85a846360528166fa18d9
IAmRustee <wewen305@gmail.com>
2025-11-21 12:26:41 +0800

    Widget Update 0.0.1-2
    Changes:
    - Changed bottom box clear button functionality into inputController.clear instead of _clearAll()
    - Mic button background now changes color when active and inactive.
    
    Additions:
    - Added Clear All button: Clears all text from both top and bottom text boxes.
    - Copy Button Functionality: Copies all text from top textbox inot clipboard.
    - Paste Button Functionality: Pastes text from clipboard into the bottom text box.
    - Cut Button Functionality: Copies and clears all text from top text box.
    - Clear All Button Functionality: Clears all text from both top and bottom text boxes.
    - Snackbar messages for certain situations in button methods.
    - _showSnackBar(String message) method to make coding snackbars more efficient.
    
    Removals:
    - Removed bubble button widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+//REMOVE WHEN TL ALGO IS PRESENT
+import 'dart:math';
+
+import 'package:flutter/services.dart'; //Placeholder for Translation alogirithm
+
 void main() {
   runApp(const MyApp());
 }
@@ -25,8 +30,9 @@ class LayoutUI extends StatefulWidget {
 
 class _LayoutUIState extends State<LayoutUI> {
   final TextEditingController inputController = TextEditingController();
+  final Random rand = Random();
 
-  //Placeholder Texts
+  //Hint texts
   String topPlaceholder = "Translated text will appear here";
   String bottomPlaceholder = "Type text to be translated here.";
 
@@ -34,8 +40,21 @@ class _LayoutUIState extends State<LayoutUI> {
   String translatedText = "";
   String osakaText = "";
 
+  //Mic variables
+
   bool micState = false;
-  void _PLACEHOLDER_METHOD() {}
+  MaterialColor micColor = Colors.blueGrey;
+
+  //PLACEHOLDER FOR TL ALGO
+  //REMOVE IN FINAL CODE --+--
+  List<String> placeholderTLStrings = [
+    'But it refused',
+    'PLACEHOLDER',
+    'What are you saying?',
+    'Idk what ur saying but this method works',
+  ];
+
+  void _PLACEHOLDER_METHOD() {} //REMOVE IN FINAL CODE --+--
   void _updateText() {
     setState(() {
       //To Do when backend is finished.
@@ -46,16 +65,82 @@ class _LayoutUIState extends State<LayoutUI> {
   //Update mic icon method when pressed
   void _updateMicState() {
     setState(() {
-      //change mic icon based on micState
+      //change mic icon and background colorbased on micState
       micState = !micState;
+      micColor = micState == false ? Colors.blueGrey : Colors.red;
     });
   }
 
+  //WIP: Call translation algo
+  void _translate() {
+    //call the backend algo for translation
+    setState(() {
+      translatedText = inputController.text.isEmpty
+          ? ""
+          : (inputController.text +
+                placeholderTLStrings[rand.nextInt(
+                  (placeholderTLStrings.length - 1),
+                )]);
+      //WIP: Replace with proper translation method call
+    });
+  }
+
+  // ----------- UTILITY METHODS ------------
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  // ----------- BUTTON FUNCTIONALITIES ------------
+  //Copy method
+  void _copyTranslatedText() {
+    if (translatedText.isNotEmpty) {
+      Clipboard.setData(ClipboardData(text: translatedText));
+      _showSnackBar('Text copied to clipboard!');
+    }
+    _showSnackBar('There is no text to copy');
+  }
+
+  //paste Method
+  void _pasteText() async {
+    final data = await Clipboard.getData(Clipboard.kTextPlain);
+    if (data != null && data.text != null && data.text!.isNotEmpty) {
+      setState(() {
+        inputController.text = data.text!;
+      });
+      _showSnackBar('Text pasted from clipboard!');
+    } else {
+      _showSnackBar('Clipboard is empty.');
+    }
+  }
+
+  //cut method
+  void _cutTranslatedText() {
+    if (translatedText.isNotEmpty) {
+      Clipboard.setData(ClipboardData(text: translatedText));
+      _showSnackBar('Text copied to clipboard!');
+    }
+    _showSnackBar('There is no text to cut');
+    setState(() {
+      translatedText = "";
+    });
+  }
+
+  //Clear both translated text and inputController
+  void _clearAll() {
+    setState(() {
+      inputController.clear();
+      translatedText = "";
+    });
+  }
+
+  // ----------- FRONTENT CODE ------------
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final double buttonSize = size.width * 0.11;
-    final double textSize = size.width * 0.05;
+    final double textSize = size.width * 0.04;
 
     return Scaffold(
       backgroundColor: const Color(0xFF2D1242),
@@ -78,14 +163,14 @@ class _LayoutUIState extends State<LayoutUI> {
                     borderRadius: BorderRadius.circular(14),
                   ),
                   child: TextField(
-                    controller: null,
+                    controller: TextEditingController(
+                      text: translatedText.isEmpty
+                          ? "Translated text will appear here"
+                          : translatedText,
+                    ),
+                    readOnly: true,
                     maxLines: null,
                     expands: true,
-                    textInputAction: TextInputAction.done,
-                    decoration: InputDecoration(
-                      hintText: topPlaceholder,
-                      border: InputBorder.none,
-                    ),
                     style: TextStyle(color: Colors.black, fontSize: textSize),
                   ),
                 ),
@@ -103,7 +188,7 @@ class _LayoutUIState extends State<LayoutUI> {
                       Colors.white,
                       Colors.black,
                       onTap: () {
-                        _PLACEHOLDER_METHOD();
+                        _pasteText();
                       },
                     ),
                     SizedBox(width: size.width * 0.03),
@@ -114,7 +199,7 @@ class _LayoutUIState extends State<LayoutUI> {
                       Colors.white,
                       Colors.black,
                       onTap: () {
-                        _PLACEHOLDER_METHOD();
+                        _copyTranslatedText();
                       },
                     ),
                     SizedBox(width: size.width * 0.03),
@@ -125,7 +210,18 @@ class _LayoutUIState extends State<LayoutUI> {
                       Colors.white,
                       Colors.black,
                       onTap: () {
-                        _PLACEHOLDER_METHOD();
+                        _cutTranslatedText();
+                      },
+                    ),
+                    SizedBox(width: size.width * 0.03),
+                    _rippleButton(
+                      //Clear All button
+                      Icons.clear_all,
+                      buttonSize,
+                      Colors.white,
+                      Colors.black,
+                      onTap: () {
+                        _clearAll();
                       },
                     ),
                   ],
@@ -173,17 +269,18 @@ class _LayoutUIState extends State<LayoutUI> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   _rippleButton(
+                    //Import audio file button
                     Icons.add,
                     buttonSize,
                     Colors.white,
                     Colors.black,
                   ),
-                  SizedBox(width: size.width * 0.05), //Import audio file button
+                  SizedBox(width: size.width * 0.05),
                   _rippleButton(
                     //Mic button
                     micState == false ? Icons.mic_off : Icons.mic,
                     buttonSize * 1.5,
-                    Colors.red,
+                    micColor,
                     Colors.white,
                     onTap: () {
                       _updateMicState();
@@ -191,13 +288,13 @@ class _LayoutUIState extends State<LayoutUI> {
                   ),
                   SizedBox(width: size.width * 0.05),
                   _rippleButton(
-                    //Submit button
+                    //Confirm button
                     Icons.check,
                     buttonSize,
                     Colors.white,
                     Colors.black,
                     onTap: () {
-                      _updateText();
+                      _translate();
                     },
                   ),
                 ],
@@ -210,15 +307,7 @@ class _LayoutUIState extends State<LayoutUI> {
   }
 }
 
-Widget _bubble(IconData icon, double size, Color background, Color iconColor) {
-  return Container(
-    width: size,
-    height: size,
-    decoration: BoxDecoration(color: background, shape: BoxShape.circle),
-    child: Icon(icon, size: size * 0.55, color: iconColor),
-  );
-}
-
+// ----------- WIDGETS ------------
 Widget _rippleButton(
   IconData icon,
   double size,
